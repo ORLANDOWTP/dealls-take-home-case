7225af36679809f78f9371f7d704beb50f9042f5
Orlando Keni Sentana <orlandokenisentana.25@gmail.com>
2024-10-07 00:48:33 +0700

    Added Swapping Classname for certain url

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,14 @@ import {
   DisclosureButton,
   DisclosurePanel,
 } from "@headlessui/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar: React.FC = () => {
+  const location = useLocation(); // Hook to get current URL
+
+  // Define your current paths to switch className
+  const isHome = location.pathname === "/";
+  const isArticles = location.pathname === "/articles";
   return (
     <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
@@ -55,14 +60,22 @@ const Navbar: React.FC = () => {
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                   <Link
                     to="/"
-                    className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    className={`${
+                      isHome
+                        ? "border-indigo-500 text-gray-900"
+                        : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
                   >
                     Home
                   </Link>
                   <Link
                     to="/articles"
                     state={{ categoryId: null }}
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    className={`${
+                      isArticles
+                        ? "border-indigo-500 text-gray-900"
+                        : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
                   >
                     All Articles
                   </Link>
